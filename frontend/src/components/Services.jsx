eb90a75536eb54df6b80390a6e68a28b1e0bdfc7
sakir456 <shaikhsakir1015@gmail.com>
2025-01-30 22:38:26 +0530

    Refactor Services component layout for improved structure and readability

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -6,7 +6,8 @@ const Services = () => {
   const navigate = useNavigate()
     
     return (
-        <div className="min-h-screen xl:px-48 md:py-48 md:px-32 lg:px-32 sm:px-28 max-sm:px-4 max-sm:py-16  px-9 py-16   mt-16  bg-[url('/src/assets/gallery/section_bg01.png')] bg-fixed  bg-cover   bg-no-repeat font-teko flex  flex-col gap-24 justify-center   ">
+        <div className="min-h-screen xl:px-48 md:py-48 md:px-32 lg:px-32 sm:px-28 max-sm:px-4 max-sm:py-16  px-9 py-16   mt-16  bg-[url('/src/assets/gallery/section_bg01.png')] bg-fixed  bg-cover   bg-no-repeat font-teko flex justify-center    ">
+        <div className="flex  flex-col gap-24 justify-center">
           <div className="flex flex-col    gap-8 max-sm:gap-3">
           <SectionHeader title="Our Services For You" textColor="text-primary" bgColor="bg-primary" />
           <p className="flex flex-col flex-wrap  text-white justify-start font-bold md:text-5xl text-4xl max-sm:text-3xl uppercase  ">PUSH YOUR LIMITS FORWARD
@@ -23,7 +24,7 @@ const Services = () => {
                   <p className="font-barlow flex flex-wrap justify-center">{item.description}</p>
                   <div
                 className="absolute   transform scale-y-0 
-              origin-bottom transition-transform duration-500 ease-in-out group-hover:scale-y-100 flex justify-center cursor-pointer gap-2 text-lg   bottom-0 w-full h-14 pt-3 px-3  bg-primary text-white "
+              origin-bottom transition-transform duration-500 ease-in-out group-hover:scale-y-100 flex justify-center cursor-pointer  text-lg   bottom-0 w-full h-14 pt-3   bg-primary text-white "
               >
               <p className="text-lg flex justify-center gap-3" onClick={()=>navigate("/contact")}>Contact Us <p>➔</p>  </p>
               </div>
@@ -31,6 +32,7 @@ const Services = () => {
               ))
             }
           </div>  
+        </div>
         </div>
     );
 };
